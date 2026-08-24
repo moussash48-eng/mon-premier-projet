d17b5a1a31c1082999c24fc8879c67efb5777f08
moussash48-eng <moussash48@gmail.com>
2026-08-24 18:43:27 +0100

    Change static file directory and update index path
    Updated static file directory from 'static' to 'public' and modified the path for serving index.html.

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// إعداد مجلد الملفات الثابتة (CSS, JS)
-app.use(express.static(path.join(__dirname, 'static')));
+// إعداد مجلد الملفات الثابتة (هنا غنردو السمية public حيت تما فين حاط ملفاتك)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // المسار الرئيسي لعرض الصفحة
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+    // حتى هنا غنقولو ليه يقلب على index.html وسط public
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // تشغيل السيرفر على البور 3000
